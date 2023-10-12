79b0f5d7df81a98dddd0afb5c5368ba054e4374b
Pedro Delfino <135232838+LidoFen@users.noreply.github.com>
2023-10-12 23:30:43 +0100

    Update modelUser.php

--- package/dist/php/modelUser.php
+++ package/dist/php/modelUser.php
@@ -12,62 +12,30 @@ class User{
 
     }
 
-    function registerUser($nome, $tipo, $telemovel, $nif, $morada, $codP, $local, $email, $pass) {
-        global $conn;
+    function registarUser($nome, $tipo, $telemovel, $nif, $morada, $codP, $local, $email, $pass){
+        global $conn; 
         $flag = true;
         $msg = "";
-        
-        // Prepare the first SQL statement
-        $sql = "INSERT INTO user(tipo_user, nome, telemovel, email, nif, morada, codigo_postal) VALUES (?, ?, ?, ?, ?, ?, ?)";
-        $stmt = $conn->prepare($sql);
-        
-        if ($stmt) {
-            // Bind parameters to the statement
-            $stmt->bind_param("isisiss", $tipo, $nome, $telemovel, $email, $nif, $morada, $codP);
-            
-            // Execute the statement
-            if ($stmt->execute()) {
-                $id = $stmt->insert_id;
-                
-                // Prepare the second SQL statement
-                $sql2 = "INSERT INTO login(id_user, password) VALUES (?, ?)";
-                $stmt2 = $conn->prepare($sql2);
-                
-                if ($stmt2) {
-                    // Bind parameters to the second statement
-                    $stmt2->bind_param("is", $id, $pass);
-                    
-                    // Execute the second statement
-                    if ($stmt2->execute()) {
-                        $msg .= "Registado com sucesso!";
-                    } else {
-                        $msg .= "Registado com sucesso mas sem credenciais de login!";
-                    }
-                } else {
-                    $msg = "Error in the login statement: " . $stmt2->error;
-                    $flag = false;
-                }
-            } else {
-                $msg = "Error in the user statement: " . $stmt->error;
-                $flag = false;
+        $sql = "INSERT INTO user(tipo_user, nome, telemovel, email, nif, morada, codigo_postal) VALUES ('".$tipo."', '".$nome."', '".$telemovel."', '".$email."', '".$nif."', '".$morada."', '".$codP."')";
+        if ($conn->query($sql) === TRUE) {
+            $id = mysqli_insert_id($conn);
+            $sql2 ="INSERT INTO login(id_user, password) VALUES ('".$id."', '".$pass."')";
+            if($conn->query($sql2) === TRUE){
+                $msg .= "Registado com sucesso!";
+            }else{
+                $msg .= "Registado com sucesso mas sem credenciais de login!"; 
             }
             
-            // Close the prepared statements
-            $stmt->close();
-            if (isset($stmt2)) {
-                $stmt2->close();
-            }
-        } else {
-            $msg = "Error in the user statement: " . $conn->error;
+        }else {
+            $msg = "Error: " . $sql . "<br>" . $conn->error;
             $flag = false;
         }
-        
+
         $resp = json_encode(array(
             "flag" => $flag,
             "msg" => $msg
         ));
-        
-        return $resp;
+        return ($resp);
     }
 }
 
