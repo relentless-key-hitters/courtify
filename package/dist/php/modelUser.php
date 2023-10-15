187f66363b18c775bde33f63cb20992a33ac2568
mpp23 <135233039+mpp23@users.noreply.github.com>
2023-10-15 12:49:36 +0100

    registo concluído

--- package/dist/php/modelUser.php
+++ package/dist/php/modelUser.php
@@ -42,24 +42,44 @@ class User{
         global $conn; 
         $flag = true;
         $msg = "";
-        $sql = "INSERT INTO user(tipo_user, nome, telemovel, email, nif, morada, codigo_postal) VALUES ('".$tipo."', '".$nome."', '".$telemovel."', '".$email."', '".$nif."', '".$morada."', '".$codP."')";
-        if ($conn->query($sql) === TRUE) {
-            $id = mysqli_insert_id($conn);
-            $sql2 ="INSERT INTO login(id_user, password) VALUES ('".$id."', '".$pass."')";
-            if($conn->query($sql2) === TRUE){
-                $msg .= "Registado com sucesso!";
-            }else{
-                $msg .= "Registado com sucesso mas sem credenciais de login!"; 
-            }
-            
-        }else {
-            $msg = "Error: " . $sql . "<br>" . $conn->error;
-            $flag = false;
+        $icon = "success";
+        if($local == 'null'){
+            $sql = "INSERT INTO user(tipo_user, nome, telemovel, email, nif, morada, codigo_postal) VALUES ('".$tipo."', '".$nome."', '".$telemovel."', '".$email."', '".$nif."', '".$morada."', '".$codP."')";
+        }else{
+            $sql = "INSERT INTO user(tipo_user, nome, telemovel, email, nif, morada, codigo_postal, localidade) VALUES ('".$tipo."', '".$nome."', '".$telemovel."', '".$email."', '".$nif."', '".$morada."', '".$codP."', '".$local."')";
         }
+        $sql2 = "SELECT user.id FROM user WHERE user.email = '".$email."'";
+        $sql3 = "SELECT user.id FROM user WHERE user.nif = '".$nif."'";
+        $result = $conn->query($sql2);
+        $result2 = $conn->query($sql3);
+
+        if ($result->num_rows > 0){
+            $msg = "Já existe uma conta com este email associado!"; 
+            $icon = "error";
+        }else if($result2->num_rows > 0){
+            $msg = "Já existe uma conta com este nif associado!"; 
+            $icon = "error";
+        }else{
+            if ($conn->query($sql) === TRUE) {
+                $id = mysqli_insert_id($conn);
+                $sql2 ="INSERT INTO login(id_user, password) VALUES ('".$id."', '".$pass."')";
+                if($conn->query($sql2) === TRUE){
+                    $msg .= "Registado com sucesso!";
+                }else{
+                    $msg .= "Registado com sucesso mas sem credenciais de login!"; 
+                }
+                
+            }else {
+                $msg = "Error: " . $sql . "<br>" . $conn->error;
+                $flag = false;
+            }
+        }
+
 
         $resp = json_encode(array(
             "flag" => $flag,
-            "msg" => $msg
+            "msg" => $msg,
+            "icon" => $icon
         ));
         return ($resp);
     }
