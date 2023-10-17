221422a440b90b6048d6d9c091145fccb705fcb0
Pedro Delfino <135232838+LidoFen@users.noreply.github.com>
2023-10-17 21:28:03 +0100

    update anims

--- landingpage/dist/js/landingpage.js
+++ landingpage/dist/js/landingpage.js
@@ -1,47 +1,51 @@
+let isDropdownOpen2 = false;
+
 function toggleDropdown() {
-    const dropdownContent = document.getElementById('dropdownContent');
-    const icon = document.getElementById('icon');
-    
-    dropdownContent.classList.toggle('hidden');
-    
-    if (dropdownContent.classList.contains('hidden')) {
-      icon.classList.remove('ti-arrow-badge-down');
-      icon.classList.add('ti-arrow-badge-right');
-    } else {
-      icon.classList.remove('ti-arrow-badge-right');
-      icon.classList.add('ti-arrow-badge-down');
-    }
+  const dropdownContent = document.getElementById('dropdownContent');
+  const icon = document.getElementById('icon');
+
+  if (!isDropdownOpen2) {
+    icon.style.animation = 'rotatePlus2 0.3s forwards';
+  } else {
+    icon.style.animation = 'rotatePlusReverse2 0.3s forwards';
   }
+
+  dropdownContent.classList.toggle('hidden');
+  isDropdownOpen2 = !isDropdownOpen2;
+}
+
+  let isDropdownOpen = false;
 
   function toggleDropdown1() {
     const dropdownContent = document.getElementById('dropdownContent1');
     const icon = document.getElementById('icon1');
-    
-    dropdownContent.classList.toggle('hidden');
-    
-    if (dropdownContent.classList.contains('hidden')) {
-      icon.classList.remove('ti-arrow-badge-down');
-      icon.classList.add('ti-arrow-badge-right');
+  
+    if (!isDropdownOpen) {
+      icon.style.animation = 'rotatePlus 0.3s forwards';
     } else {
-      icon.classList.remove('ti-arrow-badge-right');
-      icon.classList.add('ti-arrow-badge-down');
+      icon.style.animation = 'rotatePlusReverse 0.3s forwards';
     }
+  
+    dropdownContent.classList.toggle('hidden');
+    isDropdownOpen = !isDropdownOpen;
   }
+
+  let isDropdownOpen3 = false;
 
   function toggleDropdown2() {
     const dropdownContent = document.getElementById('dropdownContent2');
     const icon = document.getElementById('icon2');
-    
-    dropdownContent.classList.toggle('hidden');
-    
-    if (dropdownContent.classList.contains('hidden')) {
-      icon.classList.remove('ti-arrow-badge-down');
-      icon.classList.add('ti-arrow-badge-right');
+  
+    if (!isDropdownOpen3) {
+      icon.style.animation = 'rotatePlus3 0.3s forwards';
     } else {
-      icon.classList.remove('ti-arrow-badge-right');
-      icon.classList.add('ti-arrow-badge-down');
+      icon.style.animation = 'rotatePlusReverse3 0.3s forwards';
     }
+  
+    dropdownContent.classList.toggle('hidden');
+    isDropdownOpen3 = !isDropdownOpen3;
   }
+  
 
 
   const btn = document.getElementById('button');
