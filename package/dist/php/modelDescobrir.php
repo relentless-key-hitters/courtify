b384c3b598266b65e322291703131b8ad1a5234f
Pedro Delfino <135232838+LidoFen@users.noreply.github.com>
2023-12-02 15:39:09 +0000

    Update modelDescobrir.php

--- package/dist/php/modelDescobrir.php
+++ package/dist/php/modelDescobrir.php
@@ -61,7 +61,8 @@ class Descobrir {
                 tipo_campo ON campo.tipo_campo = tipo_campo.id
                 INNER JOIN
                 concelho ON user.localidade = concelho.id
-                WHERE listagem_atletas_marcacao.votacao = '2'
+                WHERE marcacao.tipo = 'aberta'
+                AND listagem_atletas_marcacao.votacao = '2'
                 AND concelho.descricao = '".$localidadeUserLogin."';";
 
         
