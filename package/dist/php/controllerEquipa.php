289b3f6a21acac81ff3bda0c653b474f081e2b44
luisrama <luis_r_15@outlook.com>
2024-01-08 16:54:32 +0000

    Revert "CRUD equipa + fix"
    This reverts commit 941de90a9d0cc87abbe04f3a6174b6a8756b2f1e.

--- package/dist/php/controllerEquipa.php
+++ package/dist/php/controllerEquipa.php
@@ -7,49 +7,5 @@ $equipa = new Equipa();
 if($_POST['op'] == 1){
     $res = $equipa -> getEquipasUser();
     echo($res);
-
-} else if($_POST['op'] == 2){
-
-
-    $resp = $equipa -> regEquipaModel(
-        $_POST['nomeEquipa'],
-        $_FILES,
-        $_POST['nmrEquipa'],
-        $_POST['nivelEquipa'],
-        $_POST['obsEquipa']
-    );
-   
-
-    echo($resp);
-
-} else if($_POST['op'] == 3){
-
-    $resp = $equipa -> getListaEquipaModel();
-    echo($resp);
-
-} else if($_POST['op'] == 4){
-
-
-    $resp = $equipa -> getDadosEquipaModel($_POST['id']);
-    echo($resp);
-
-} else if($_POST['op'] == 5){
-
-
-    $resp = $equipa -> guardaEditEquipaModel(
-        $_POST['nomeEquipa'],
-        $_FILES,
-        $_POST['nmrEquipa'],
-        $_POST['nivelEquipa'],
-        $_POST['obsEquipa']
-
-    );
-    echo ($resp);
-
-} else if($_POST['op'] == 6){
-
-    $resp = $equipa -> removeEquipaModel($_POST['id']);
-    echo($resp);
-
 }
 ?>
