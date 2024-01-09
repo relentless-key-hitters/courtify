10f42c1d157ecfa716cf3aa0abadf23e341081c4
mpp23 <135233039+mpp23@users.noreply.github.com>
2024-01-09 15:58:51 +0000

    equipas

--- package/dist/js/js_courtify/equipa.js
+++ package/dist/js/js_courtify/equipa.js
@@ -20,3 +20,33 @@ function getEquipasUser(){
         alert("Request failed: " + textStatus);
       });
 }
+
+function getEquipasLocalidadeUser(){
+  let dados = new FormData();
+  dados.append("op", 2);
+
+  $.ajax({
+    url: "../../dist/php/controllerEquipa.php",
+    method: "POST",
+    data: dados,
+    dataType: "html",
+    cache: false,
+    contentType: false,
+    processData: false,
+  })
+
+    .done(function (msg) {
+      $("#equipasLocalidade").html(msg);
+    })
+
+    .fail(function (jqXHR, textStatus) {
+      alert("Request failed: " + textStatus);
+    });
+
+
+}
+
+$(function () {
+  getEquipasUser();
+  getEquipasLocalidadeUser();
+});
--- package/dist/php/controllerEquipa.php
+++ package/dist/php/controllerEquipa.php
@@ -7,5 +7,18 @@ $equipa = new Equipa();
 if($_POST['op'] == 1){
     $res = $equipa -> getEquipasUser();
     echo($res);
+}else if($_POST['op'] == 2){
+    $res = $equipa -> getEquipasLocalidadeUser();
+    echo($res);
+}else if($_POST['op'] == 3){
+    $pagina = isset($_POST['pagina']) ? intval($_POST['pagina']) : 1;
+    $porPagina = 12; // Número de resultados por página
+    $offset = ($pagina - 1) * $porPagina;
+
+    $res = $equipa -> getAtletasEquipa($_POST['id'], $offset, $porPagina);
+    echo($res);
+}else if($_POST['op'] == 4){
+    $res = $equipa -> getInfoEquipa($_POST['id']);
+    echo($res);
 }
 ?>
